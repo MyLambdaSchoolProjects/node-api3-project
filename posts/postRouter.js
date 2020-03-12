ed635e13bdab9ad1be32b7a2115a6062bf3e1b4e
David Isakson <ikeman32@yahoo.com>
2020-03-12 11:13:38 -0700

    add env

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,27 +1,75 @@
 const express = require('express');
+const postDb = require('./postDb')
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  // do your magic!
+  // Get posts
+  postDb.get()
+    .then(posts => {
+      res.status(200).json(posts);
+    })
+    .catch(err=> {
+      res.status(500).json({message: 'There was an error retrieving posts'});
+    });
 });
 
-router.get('/:id', (req, res) => {
-  // do your magic!
+router.get('/:id', validatePostId, (req, res) => {
+  // Get post by id
+  const postId = req.params.id;
+  postDb.getById(postId)
+    .then(post => {
+      res.status(200).json(post);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({message: 'There was an error retrieving the Post'});
+    });
 });
 
-router.delete('/:id', (req, res) => {
-  // do your magic!
+router.delete('/:id', validatePostId, (req, res) => {
+  // Delete post
+  postDb.remove(req.params.id)
+    .then(post => {
+      res.status(200).json({message: `Post: ${req.params.id} Deleted`});
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({message: "There was an error Deleting the post"});
+    });
 });
 
-router.put('/:id', (req, res) => {
-  // do your magic!
+router.put('/:id', validatePostId, (req, res) => {
+  // Update post
+  const body = req.body
+  const id = req.params.id
+  if(body.text){
+    postDb.update(id, body)
+      .then(post => {
+        res.status(201).json({message: `Post updated`, post})
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json({message: 'There was an error updating the post'});
+      });
+  }else{
+    res.status(400).json({message: 'Please provide the required text and post Id'});
+  }
+  
 });
 
 // custom middleware
 
 function validatePostId(req, res, next) {
   // do your magic!
+  postDb.getById(req.params.id)
+    .then(post => {
+      if(post){
+        next();
+      }else{
+        res.status(400).json({message: "Invalid post Id"})
+      }
+    })
 }
 
 module.exports = router;
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,47 +1,140 @@
-const express = require('express');
+const express = require("express");
+const userDb = require("./userDb");
+const postDb = require("../posts/postDb");
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
-  // do your magic!
+router.post("/", validateUser, (req, res) => {
+  //Create new user
+  userDb
+    .insert(req.body)
+    .then(user => {
+      res.status(201).json(user);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ message: "There was an error creating user." });
+    });
 });
 
-router.post('/:id/posts', (req, res) => {
-  // do your magic!
+router.post("/:id/posts", validatePost, validateUserId, (req, res) => {
+  // Create new post
+  postDb
+    .insert(req.body)
+    .then(post => {
+      res.status(201).json(post);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ message: "There was an error creating the post" });
+    });
 });
 
-router.get('/', (req, res) => {
-  // do your magic!
+router.get("/", (req, res) => {
+  // Get users
+  userDb
+    .get()
+    .then(users => {
+      res.status(200).json(users);
+    })
+    .catch(err => {
+      res.status(500).json({ message: "There was an error retrieving users" });
+    });
 });
 
-router.get('/:id', (req, res) => {
-  // do your magic!
+router.get("/:id", validateUserId, (req, res) => {
+  // Get users by id
+  userDb
+    .getById(req.params.id)
+    .then(user => {
+      res.status(200).json(user);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ message: "There was and error." });
+    });
 });
 
-router.get('/:id/posts', (req, res) => {
-  // do your magic!
+router.get("/:id/posts", validateUserId, (req, res) => {
+  // Get post by user id
+  userDb
+    .getUserPosts(req.params.id)
+    .then(posts => {
+      res.status(200).json(posts);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ message: "There was an error retrieving posts" });
+    });
 });
 
-router.delete('/:id', (req, res) => {
-  // do your magic!
+router.delete("/:id", validateUserId, (req, res) => {
+  // Delete user
+  userDb
+    .remove(req.params.id)
+    .then(post => {
+      res.status(200).json({ message: `User: ${req.params.id} Deleted` });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ message: "There was an error Deleting the post" });
+    });
 });
 
-router.put('/:id', (req, res) => {
-  // do your magic!
+router.put("/:id", validateUserId, (req, res) => {
+  // Update user
+  const body = req.body;
+  const id = req.params.id;
+  if (body.name) {
+    userDb
+      .update(id, body)
+      .then(user => {
+        res.status(201).json({ message: `Post updated`, user });
+      })
+      .catch(err => {
+        console.log(err);
+        res
+          .status(500)
+          .json({ message: "There was an error updating the user" });
+      });
+  } else {
+    res
+      .status(400)
+      .json({ message: "Please provide the required name and user Id" });
+  }
 });
 
 //custom middleware
 
 function validateUserId(req, res, next) {
   // do your magic!
+  userDb.getById(req.params.id).then(id => {
+    if (id) {
+      next();
+    } else {
+      res.status(400).json({ message: "Invalid user Id" });
+    }
+  });
 }
 
 function validateUser(req, res, next) {
   // do your magic!
+  if (!req.body) {
+    res.status(400).json({ messag: "Missing required user information" });
+  } else if (!req.body.name) {
+    res.status(400).json({ message: "Missing required name field" });
+  } else {
+    next();
+  }
 }
 
 function validatePost(req, res, next) {
   // do your magic!
+  if (!req.body.text) {
+    res.status(400).json({ message: "Missing required text field" });
+  } else {
+    next();
+  }
 }
 
 module.exports = router;
